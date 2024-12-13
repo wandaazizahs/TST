0d163ea0382a5ab25c626ac2d9cf24b92a244eb2
wandaazizahs <133151477+wandaazizahs@users.noreply.github.com>
2024-12-13 17:09:44 +0700

    Add files via upload

---
+++ bar/footer.php
@@ -0,0 +1,8 @@
+<footer class="app-footer"> <!--begin::To the end-->
+            <div class="float-end d-none d-sm-inline">SMK Negeri 2 Malang</div> <!--end::To the end--> <!--begin::Copyright--> <strong>
+            Hak Cipta &copy; <?php echo date('Y-m-d'); ?>&nbsp;
+                <a href="https://smkn2malang.sch.id/" class="text-decoration-none">SMKN 2 Malang</a>.
+            </strong>
+            Dikembangkan oleh Tim ICT.
+            <!--end::Copyright-->
+</footer> <!--end::Footer-->
